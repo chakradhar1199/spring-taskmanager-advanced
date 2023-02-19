33fbd55d88e1f7210178b823724e5d43f61dce81
chakri reddy <chakri11099@gmail.com>
2023-02-19 11:12:44 +0530

    spring task manager advanced

---
+++ src/main/java/com/example/springtaskmanageradvanced/SpringTaskmanagerAdvancedApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtaskmanageradvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskmanagerAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTaskmanagerAdvancedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springtaskmanageradvanced/SpringTaskmanagerAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtaskmanageradvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskmanagerAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
